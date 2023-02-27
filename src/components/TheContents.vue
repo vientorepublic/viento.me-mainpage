29c0d5c131d1277e9845dcc98bb683097c5cc63d
Viento <op@viento.me>
2023-02-27 21:03:53 +0900

    ㅁㄴㅇㄹ

--- src/components/TheContents.vue
+++ src/components/TheContents.vue
@@ -3,12 +3,12 @@
 	<div class="detail">
 			<h2 class="subsubtitle">Skils</h2>
 			<hr/>
-            <img alt="Vue.js" src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D" style="margin: 3px">
-            <img alt="HTML5" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" style="margin: 3px">
-            <img alt="Javascript" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" style="margin: 3px">
-            <img alt="CSS3" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" style="margin: 3px">
-            <img alt="MYSQL" src="https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white" style="margin: 3px">
-			<img alt="C++" src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white" style="margin: 3px">
+        <img alt="Vue.js" src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D" style="margin: 3px">
+        <img alt="HTML5" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" style="margin: 3px">
+        <img alt="Javascript" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" style="margin: 3px">
+        <img alt="CSS3" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" style="margin: 3px">
+        <img alt="MYSQL" src="https://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white" style="margin: 3px">
+				<img alt="C++" src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white" style="margin: 3px">
 			<img alt="Shell Script" src="https://img.shields.io/badge/Shell_Script-121011?style=for-the-badge&logo=gnu-bash&logoColor=white" style="margin: 3px">
 			<img alt="Express.js" src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" style="margin: 3px">
     </div>
@@ -19,11 +19,14 @@
 				<a href="https://api.viento.me/quote" target="_blank">
 					<li>명언 API</li>
 				</a>
-				<a href="https://api.viento.me/tjchart" target="_blank">
-					<li>TJ 음악 차트 API</li>
+				<a href="https://api.viento.me/melonChart" target="_blank">
+					<li>멜론차트 API</li>
 				</a>
-				<a href="https://api.viento.me/nsfw" target="_blank">
-					<li>NSFW Image API</li>
+				<a href="https://api.viento.me/melonLatest" target="_blank">
+					<li>멜론최신 API</li>
+				</a>
+				<a href="https://api.viento.me/melonSearch" target="_blank">
+					<li>멜론검색 API</li>
 				</a>
 			</div>
 			<p style="display: inline-block;font-size:1.5rem;">자세한 사용 가이드는</p>
@@ -34,42 +37,12 @@
 			<h2 class="subsubtitle">Minecraft</h2>
 			<hr/>
 			<iframe style="width:728px;height:90px;max-width:100%;border:none;display:block;margin:auto" src="https://ko.namemc.com/server/mc.viento.me/embed" width="728" height="90"></iframe>
-			<div class="link">
-				<a href="https://map.viento.me" target="_blank">
-					<li>SMP 서버맵</li>
-				</a>
-				<a href="https://discord.gg/NhGKz6NS2y" target="_blank">
-					<li>공식 디스코드</li>
-				</a>
-			</div>
-		</div>
-		<div class="detail">
-			<h2 class="subsubtitle">Mirror</h2>
-			<hr/>
-			<div class="link">
-				<a href="https://mirror.viento.me" target="_blank">
-					<li>비엔토 미러</li>
-				</a>
-			</div>
-			<p style="display: inline-block;font-size:1.5rem;">비엔토 미러는 자유/공개 소프트웨어 비영리 미러링 서비스 입니다. 주요 사용 국가는 대한민국, 인접한 동아시아 국가입니다.</p>
-		</div>
-		<div class="detail">
-			<h2 class="subsubtitle">Participating Projects</h2>
-			<hr/>
-			<div class="link">
-				<a href="https://github.com/Devlobots-org" target="_blank">
-					<li>데브로봇스 위키, 디키</li>
-				</a>
-				<a href="https://gprotect.xyz" target="_blank">
-					<li>Guilds Protect</li>
-				</a>
-			</div>
 		</div>
 		<div class="detail">
 			<h2 class="subsubtitle">Information</h2>
 			<hr/>
 			<div class="link">
-				<a href="https://status.viento.me" target="_blank">
+				<a href="https://stats.uptimerobot.com/3jJxocXEzA" target="_blank">
 					<li>Server Status</li>
 				</a>
 			</div>
